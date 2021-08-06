42061aa130ff527e76d3f50c1cfd393b9fad0ff3
Diego <diegoteixeirapereira@hotmail.com>
2021-08-06 17:34:22 -0300

    criação dos arquivos modelo java.

---
+++ src/main/java/br/com/springbackend/model/Livro.java
@@ -0,0 +1,65 @@
+package br.com.springbackend.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "livro")
+public class Livro {
+    
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nome")
+    private String nome;
+
+    @Column(name = "autor")
+    private String autor;
+
+    @Column(name = "preco")
+    private String preco;
+
+    @Column(name = "picByte", length = 1000)
+    private byte[] picByte;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getPreco() {
+        return preco;
+    }
+
+    public void setPreco(String preco) {
+        this.preco = preco;
+    }
+
+    public byte[] getPicByte() {
+        return picByte;
+    }
+
+    public void setPicByte(byte[] picByte) {
+        this.picByte = picByte;
+    }
+}
---
+++ src/main/java/br/com/springbackend/model/Usuario.java
@@ -0,0 +1,54 @@
+package br.com.springbackend.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "usuario")
+public class Usuario {
+    
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nome")
+    private String nome;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "tipo")
+    private String tipo;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+}
